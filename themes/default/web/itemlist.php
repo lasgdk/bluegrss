4e2aa651bbe45c14ad245e9a907a356da9d9dd8e
lasgdk <lasg@lasg.dk>
2019-09-05 13:55:19 +0200

    Add files via upload

--- themes/default/web/itemlist.php
+++ themes/default/web/itemlist.php
@@ -1,16 +1,19 @@
 <?php if (rss_itemlist_has_extractions()) { ?>
+<div class="feedaction withmargin">
+	<?php rss_itemlist_prerender_callback(); ?>
+</div>
 <?php } ?>
 <?php if ($title = rss_itemlist_title()) { ?>
 <h2<?php echo rss_itemlist_anchor(); ?>>
 <?php if ($icon = rss_itemlist_icon()) { ?>
 	<img src="<?php echo $icon; ?>" class="favicon" alt="" />
 <?php } ?>
-	<?php //echo $title; ?> 
+	<?php echo $title; ?> 
 </h2>
 <?php } else { ?>
-<a <?php //echo rss_itemlist_anchor(); ?>></a>
+<a <?php echo rss_itemlist_anchor(); ?>></a>
 <?php } ?>
-<?php //echo rss_itemlist_before_list(); ?>
+<?php echo rss_itemlist_before_list(); ?>
 <?php rss_itemlist_navigation(); ?>
 <?php rss_itemlist_feeds(); ?>
 <?php rss_itemlist_navigation(); ?>
